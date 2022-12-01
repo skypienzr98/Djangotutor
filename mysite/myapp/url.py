0bfc3fb3d9cc451f3f275c38cacc7197437860d8
skypienzr98 <117338905+skypienzr98@users.noreply.github.com>
2022-12-01 13:03:26 +0800

    Update url.py

--- mysite/myapp/url.py
+++ mysite/myapp/url.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bands/', views.band_listing, name='band-list'),
-    path('bands/<int:band_id>/', views.band_detail, name='band-detail'),
-    path('bands/search/', views.band_search, name='band-search'),
+    path('', views.index, name='index'),
 ]
